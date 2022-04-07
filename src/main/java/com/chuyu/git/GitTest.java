b751f7b2dc6761cff8ed854eba29cb0e2f2a25da
chuyu <chuyu@weishang.com>
2022-04-07 08:49:59 +0800

    first commit

---
+++ src/main/java/com/chuyu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.chuyu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-04-06 19:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
